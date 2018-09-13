3c746cc0cc21e846107e8829033c55f8516f8eca
BingyuZhou <zbyjeff@gmail.com>
2018-09-13 14:06:42 +0200

    insertion sort

---
+++ insert_sort/src.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class insert_sort{
+    public:
+    insert_sort(){};
+
+    void sort(vector<int> &seq){
+        int length = seq.size();
+
+        for(int i=1; i<length; ++i){
+            int key = seq[i];
+        int j = i-1;
+            while(j>=0 && seq[j]>key){
+                seq[j+1] = seq[j];
+                --j;
+            }
+            seq[j+1] = key;
+        }
+    }
+
+    ~insert_sort(){};
+
+};
